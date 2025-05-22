526e476e0627f72213b03cd2a3fd193b95c818f0
Hector Raya <ganchodemar.11@gmail.com>
2025-05-22 15:36:18 -0600

    dashboard check userkey

--- sites/scripts/dashboard.js
+++ sites/scripts/dashboard.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
      const userKey = sessionStorage.getItem('userKey');
      if (!userKey) {
-          window.location.href = '/login.html';
+          window.location.href = 'login.html';
      } 
 });
